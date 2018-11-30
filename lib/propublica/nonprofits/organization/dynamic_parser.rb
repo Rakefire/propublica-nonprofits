01a81839ddb62aa42667963dbe9bf234f3230f6d
Ricky Chilcott <ricky@rakefire.io>
2018-11-30 08:41:24 -0500

    Refactor error handling to be more efficient

--- lib/propublica/nonprofits/organization/dynamic_parser.rb
+++ lib/propublica/nonprofits/organization/dynamic_parser.rb
@@ -7,11 +7,7 @@ module Propublica
           fields.each do |field|
             define_method field do
               vars = self.instance_variable_get("@attributes")
-              vars.fetch(field.to_s) do
-                class_name = self.class.to_s.split("::").last
-                raise Propublica::Nonprofits::DataNotFetched,
-                  "#{class_name}##{field} not fetched from API. This may be due to an API error or because you tried to access a Basic property on the full results"
-              end
+              vars.fetch(field.to_s) { raise_field_fetch_error(field) }
             end
           end
         end
@@ -27,6 +23,12 @@ module Propublica
         private
 
         attr_reader :attributes
+
+        def raise_field_fetch_error(field)
+          class_name = self.class.to_s.split("::").last
+          raise Propublica::Nonprofits::DataNotFetched,
+            "#{class_name}##{field} not fetched from API. This may be due to an API error or because you tried to access a Basic property on the full results"
+        end
       end
     end
   end
